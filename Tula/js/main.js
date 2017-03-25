9ae5a96c8a2611245e0855588f4a4d7230bb2ccd
VitaliyZt <vitaliyzt@mail.ua>
2017-03-25 10:58:52 +0200

    Tula2503

--- Tula/js/main.js
+++ Tula/js/main.js
@@ -2,6 +2,29 @@ $(function(){
 	$(".s1-slider").slick({	slidesToShow: 1	});
 	$(".s2-blocks").slick({	slidesToShow: 3	});
 	$(".s3-slider").slick({	slidesToShow: 1	});
+	
+
+
+
+
+		$("#time-1").ionRangeSlider({
+			keyboard: true,
+			min: 1,
+			max: 120,
+			from: 5,
+			step: 1,
+			postfix: " дн."
+		});
+		$("#time-2").ionRangeSlider({
+			keyboard: true,
+			min: 1,
+			max: 120,
+			from: 7,
+			step: 1,
+			postfix: " ч."
+		});
+
+
 });
 
 
