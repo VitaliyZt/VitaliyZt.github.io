c392af4b791a4fd2b019ae05e4c276bbf015426c
VitaliyZt <vitaliyzt@mail.ua>
2017-03-26 15:37:15 +0300

    Tula2603

--- Tula/js/main.js
+++ Tula/js/main.js
@@ -1,29 +1,54 @@
 $(function(){
-	$(".s1-slider").slick({	slidesToShow: 1	});
-	$(".s2-blocks").slick({	slidesToShow: 3	});
+	$(".s1-slider").slick({	slidesToShow: 1	});	
+	
+	$(".s2-blocks").slick({
+		slidesToShow: 3,
+		responsive: [{
+		breakpoint: 960,
+		settings: {	slidesToShow: 1	}
+		}]
+	});	
+	
 	$(".s3-slider").slick({	slidesToShow: 1	});
 	
+	$(".s4-blocks").slick({
+		slidesToShow: 3,
+		responsive: [{
+		breakpoint: 960,
+		settings: {	slidesToShow: 1	}
+		}]
+	});	
 
+	$("#time-1").ionRangeSlider({
+		keyboard: true,
+		min: 1,
+		max: 120,
+		from: 5,
+		step: 1,
+		postfix: " дн."
+	});
+	$("#time-2").ionRangeSlider({
+		keyboard: true,
+		min: 1,
+		max: 120,
+		from: 7,
+		step: 1,
+		postfix: " ч."
+	});
 
-
-
-		$("#time-1").ionRangeSlider({
-			keyboard: true,
-			min: 1,
-			max: 120,
-			from: 5,
-			step: 1,
-			postfix: " дн."
-		});
-		$("#time-2").ionRangeSlider({
-			keyboard: true,
-			min: 1,
-			max: 120,
-			from: 7,
-			step: 1,
-			postfix: " ч."
-		});
-
+	(function(){
+		function zoom(){
+			var el = $("#viewport"),
+				ww = window.screen.width, mw = 580, ratio = ww/mw;
+			if (ww<mw){ 
+				el.attr('content', 'initial-scale='+ratio+', maximum-scale='+ratio+', minimum-scale='+ratio+', user-scalable=no, width='+mw);
+			} else {
+				el.attr('content', 'initial-scale=1.0, maximum-scale=1, minimum-scale=1.0, user-scalable=yes, width='+ ww);
+			}
+		}
+		window.addEventListener('resize', function(){ zoom(); });
+		zoom();
+	}());	
 
 });
 
