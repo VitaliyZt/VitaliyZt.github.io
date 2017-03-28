b53960fdd3dd365d46caea34b71ae05dfbdd420b
VitaliyZt <vitaliyzt@mail.ua>
2017-03-29 01:22:34 +0300

    Tula2903

--- Tula/js/main.js
+++ Tula/js/main.js
@@ -27,7 +27,6 @@ $(function(){
 	});	
 
 	$("#time-1").ionRangeSlider({
-		keyboard: true,
 		min: 1,
 		max: 120,
 		from: 5,
@@ -35,13 +34,47 @@ $(function(){
 		postfix: " дн."
 	});
 	$("#time-2").ionRangeSlider({
-		keyboard: true,
 		min: 1,
 		max: 120,
 		from: 7,
 		step: 1,
 		postfix: " ч."
 	});
+	
+	$("#range-1").ionRangeSlider({
+		hide_min_max: true,
+		min: 1,
+		max: 10,
+		from: 4,
+		step: 1,
+		onChange: function(){
+			var val = $(".s8-range-box:nth-child(1) .irs-single").text();
+			$(".s8-range-box:nth-child(1) .s8-range-value").text(val);
+		}
+	});
+	$("#range-2").ionRangeSlider({
+		hide_min_max: true,
+		min: 1,
+		max: 10,
+		from: 4,
+		step: 1,
+		onChange: function(){
+			var val = $(".s8-range-box:nth-child(2) .irs-single").text();
+			$(".s8-range-box:nth-child(2) .s8-range-value").text(val);
+		}
+	});
+	$("#range-3").ionRangeSlider({
+		hide_min_max: true,
+		min: 1,
+		max: 100,
+		from: 80,
+		step: 1,
+		onChange: function(){
+			var val = $(".s8-range-box:nth-child(3) .irs-single").text();
+			$(".s8-range-box:nth-child(3) .s8-range-value").text(val);
+		}
+	});
+
 
 	(function(){
 		function zoom(){
@@ -55,7 +88,27 @@ $(function(){
 		}
 		window.addEventListener('resize', function(){ zoom(); });
 		zoom();
-	}());	
+	}());
+	
+	// яндекс карта
+	if($("#map").length){		
+		ymaps.ready(init);
+		var myMap, myPlacemark;
+		function init(){     
+			myMap = new ymaps.Map("map", {
+				center: [55.76707457, 37.56832700],
+				zoom: 16,
+				controls: []
+			});
+			myPlacemark = new ymaps.Placemark([55.76717457, 37.57132700], {
+				iconCaption: 'Малая Грузинская ул., 27/13'
+			},{
+				preset: 'islands#redDotIconWithCaption'			
+			});		
+			myMap.geoObjects.add(myPlacemark);
+			myMap.behaviors.disable('scrollZoom'); 	
+		}
+	}
 
 });
 
