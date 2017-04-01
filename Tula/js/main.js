0f36cfd0ba21fa469f38c0bbf6de38accca561ec
VitaliyZt <vitaliyzt@mail.ua>
2017-04-01 23:58:29 +0300

    Tula0104

--- Tula/js/main.js
+++ Tula/js/main.js
@@ -163,12 +163,50 @@ $(function(){
 			});
 		});
 	}
+	$(".s8-tag").on("click", function(){
+		$(this).remove();
+	});
 	$(".s13-filt-close-btn").on("click", function(){
 		$(".s13-filters").slideUp();
 	});
 	$(".s13-top-btn, .s13-top-search-btn").on("click", function(){
 		$(".s13-filters").slideDown();
 	});
+	$(".s15-filt-close-btn").on("click", function(){
+		$(".s15-filters").slideUp();
+	});
+	$(".s15-top-search-btn").on("click", function(){
+		$(".s15-filters").slideDown();
+	});
+	
+	if($(".datepicker").length){
+		$(".datepicker").datepicker({
+			numberOfMonths: [1, 1],
+			beforeShowDay: function(date) {
+				var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
+				var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
+				return [true, date1 && ((date.getTime() == date1.getTime()) || (date2 && date >= date1 && date <= date2)) ? "dp-highlight" : ""];
+			},
+			onSelect: function(dateText, inst) {
+				var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
+				var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
+				var selectedDate = $.datepicker.parseDate($.datepicker._defaults.dateFormat, dateText);
+
+				if (!date1 || date2) {
+					$("#input1").val(dateText);
+					$("#input2").val("");
+					$(this).datepicker();
+				} else if (selectedDate < date1) {
+					$("#input2").val($("#input1").val());
+					$("#input1").val(dateText);
+					$(this).datepicker();
+				} else {
+					$("#input2").val(dateText);
+					$(this).datepicker();
+				}
+			}
+		});
+	}
 
 });
 
