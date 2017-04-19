21b316f572b03620b8a8916709cd65c3759e27bf
VitaliyZt <vitaliyzt@mail.ua>
2017-04-19 20:12:50 +0300

    Naval1904

---
+++ naval/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+	$(".n2-slides").slick({
+		prevArrow: "<button class='n2-left'><i class='fa fa-chevron-left'></i></button>",
+		nextArrow: "<button class='n2-right'><i class='fa fa-chevron-right'></i></button>"
+	});
+});
