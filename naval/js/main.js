3f295acca3c496d9f2c7750c09c3c7cba94fb886
VitaliyZt <vitaliyzt@mail.ua>
2017-04-20 23:17:33 +0300

    Naval2004v2

--- naval/js/main.js
+++ naval/js/main.js
@@ -3,4 +3,11 @@ $(function(){
 		prevArrow: "<button class='n2-left'><i class='fa fa-chevron-left'></i></button>",
 		nextArrow: "<button class='n2-right'><i class='fa fa-chevron-right'></i></button>"
 	});
+	
+	$(".leftcol-menu>li>a").on("click", function(e){
+		e.preventDefault();
+		var currentBlock = $(this).attr("href");
+		var	currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({scrollTop: currentBlockOffset}, 500);
+	});
 });
