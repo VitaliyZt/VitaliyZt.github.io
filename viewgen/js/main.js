50e70643bc7e5d79df3d768c1047870db520ce62
VitaliyZt <vitaliyzt@mail.ua>
2017-06-05 20:35:47 +0300

    viewgen0506v2

--- viewgen/js/main.js
+++ viewgen/js/main.js
@@ -15,18 +15,34 @@ $(function(){
 		});
 	}
 	
-	$(".header-menu>li>a:not(:last)").on("click", function(){
-		var link = $(this),	headerHeight = $("header>nav").outerHeight(),
-		top = $(link.attr("href")).offset().top - headerHeight;		
-		$(".header-menu>li").removeClass("active");
-		link.parent().addClass("active");		
-		$("html, body").animate({scrollTop: top}, "slow");
-		return false;
-	});
-	
 	$(window).on("load resize", function(){
 		$("header").css("height", $("header>nav").outerHeight());
 	});
-
+	
+	$(".header-menu>li>a:not(:last), .footer-menu>li>a:not(:last)")
+		.on("click", function(e){
+		e.preventDefault();		
+		var link = $(this), target = $(link.attr("href")),
+			headerHeight = $("header>nav").outerHeight(),
+			targetPadding = parseInt(target.css("padding-top")) - 20,
+			top = target.offset().top - headerHeight + targetPadding;
+		$("html, body").stop().animate({scrollTop: top}, 800);
+	});	
+	$(window).on("scroll", function(){
+		$("[id^='scr']").each(function(i,el){
+			var top  = $(el).offset().top - 120,
+				bottom = top + $(el).height(),
+				scroll = $(window).scrollTop(),
+				id = $(el).attr('id');
+			if( scroll > top && scroll < bottom){
+				$(".header-menu .active").removeClass("active");
+				if($(window).scrollTop() + $(window).height() == $(document).height()) {
+					$(".header-menu a[href='#scr5']").parent().addClass("active");
+				} else {
+					$(".header-menu a[href='#"+id+"']").parent().addClass("active");
+				}
+			}
+		});
+	});
+	
 });
-
