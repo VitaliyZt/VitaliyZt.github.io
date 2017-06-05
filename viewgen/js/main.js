f67cfa0617d054a5ca2d8ab8a4aecedde4a2a51a
VitaliyZt <vitaliyzt@mail.ua>
2017-06-05 18:23:57 +0300

    viewgen0506

--- viewgen/js/main.js
+++ viewgen/js/main.js
@@ -4,8 +4,8 @@ $(function(){
 		$(".s9-stories").slick({
 			slidesToShow: 3,
 			slidesToScroll: 1,
-			autoplay: false,
-			autoplaySpeed: 2000,
+			autoplay: true,
+			autoplaySpeed: 7000,
 			prevArrow: btn,
 			nextArrow: btn,
 			responsive: [{
@@ -15,6 +15,18 @@ $(function(){
 		});
 	}
 	
+	$(".header-menu>li>a:not(:last)").on("click", function(){
+		var link = $(this),	headerHeight = $("header>nav").outerHeight(),
+		top = $(link.attr("href")).offset().top - headerHeight;		
+		$(".header-menu>li").removeClass("active");
+		link.parent().addClass("active");		
+		$("html, body").animate({scrollTop: top}, "slow");
+		return false;
+	});
 	
+	$(window).on("load resize", function(){
+		$("header").css("height", $("header>nav").outerHeight());
+	});
+
 });
 
