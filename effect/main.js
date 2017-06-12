3ed22754d0a5d255ab38d2dab447561cee5069bf
VitaliyZt <vitaliyzt@mail.ua>
2017-06-12 18:16:40 +0300

    effect1206v2

--- effect/main.js
+++ effect/main.js
@@ -2,10 +2,6 @@ $(function(){
 	var img = $(".bg-effect-img"), cls = "effected";
 	img.addClass(cls);
 	setInterval(function(){
-		if (!img.hasClass(cls)){
-			img.addClass(cls);
-		} else {
-			img.removeClass(cls);
-		}
+		img.toggleClass(cls);
 	}, 10000);
 });
