8aec8b4533be8caeb4f65e3b892b4e26bde13d1e
VitaliyZt <vitaliyzt@mail.ua>
2017-06-15 10:47:15 +0300

    parus15062017v2

--- parus/js/main.js
+++ parus/js/main.js
@@ -1,6 +1,5 @@
-$(function(){
-	
-	$("#phone-mask").mask("(099) 999-99-99");
+$(function(){	
+	$("#phone-mask").mask("(099) 999-99-99");	
 
 	$(".zoom-gallery").magnificPopup({
 		delegate: 'a',
@@ -30,4 +29,29 @@ $(function(){
 		nextArrow: "<button type='button' class='slick-next'>"+sl7_img+"</button>",
 	});
 	
+	$(".p1").parallax({imageSrc: "img/p1-bg.jpg"});
+	$(".p3-right").parallax({imageSrc: "img/p3-right.jpg"});
+	$(".p5").parallax({imageSrc: "img/p5-bg.jpg"});
+	$("footer").parallax({imageSrc: "img/footer-bg.jpg"});	
+
+	$("body").scrollspy({ target: "#scrollspy-nav", offset: 88 });
+	
+	$("a[href^='#scr']").on("click", function(e){
+		e.preventDefault();		
+		var link = $(this), target = $(link.attr("href")),
+			headerHeight = $("header>nav").outerHeight(),
+			top = target.offset().top - headerHeight;
+		$("html, body").stop().animate({scrollTop: top}, 800);
+	});		
+	
+	$(window).on("scroll", function(){
+		var nav = $("header>nav"), p1 = $(".p1");		
+		if ($(window).scrollTop() > $(".header-top").height()){
+			nav.addClass("navbar-fix");
+			p1.css("margin-top", parseInt(nav.outerHeight()) + "px");
+		} else {
+			nav.removeClass("navbar-fix");
+			p1.css("margin-top", "0");		
+		}
+	});		
 });
